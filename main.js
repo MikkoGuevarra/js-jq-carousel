7b78dbb1a77dc5db13484e0b0545079a075d4f37
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-10-27 17:45:34 +0100

    done with circles

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
         //creo una variabile che mi salva l'img corrente
         var currentImg = $('img.active');
 
+        //creo variabile per il dot che ha la classe  'fas'
+        var selected = $('.fa-circle.fas');
+        selected.toggleClass('fas far');
+
+
+
         //rimuovo class 'active' img corrente
         currentImg.removeClass('active');
         //creo condizione se img corrente ==1 add class
@@ -10,15 +16,18 @@ $(document).ready(function(){
         if (currentImg.next('img').length) {
             //add class 'active' next img
             currentImg.next('img').addClass('active');
-            $('.dots i').next().toggleClass('far fas')
+            selected.next().toggleClass('fas far');
         } else {
             $('.slide img:first-of-type').addClass('active');
+            $('.dots .fa-circle:first-of-type').toggleClass('fas far');
         }
     });
 
     $('.prev .fa-chevron-left').click(function() {
         //creo una variabile che mi salva l'img corrente
         var currentImg = $('img.active');
+        var selected = $('.fa-circle.fas');
+        selected.toggleClass('fas far');
 
         //rimuovo class 'active' img corrente
         currentImg.removeClass('active');
@@ -28,8 +37,10 @@ $(document).ready(function(){
         if (currentImg.prev('img').length) {
             //add class 'active' prev img
             currentImg.prev('img').addClass('active');
+            selected.prev().toggleClass('fas far');
         } else {
             $('.slide img:last-of-type').addClass('active');
+            $('.dots .fa-circle:last-of-type').toggleClass('fas far');
         }
     });
 
